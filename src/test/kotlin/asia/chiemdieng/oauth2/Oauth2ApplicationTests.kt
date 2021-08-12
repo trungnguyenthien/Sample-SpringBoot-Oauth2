07d2dc5ecdd6ca660934d33973db128859d3e1fc
Trung Nguyen Thien <ngthientrung@gmail.com>
2021-08-12 12:05:33 +0700

    no message

---
+++ src/test/kotlin/asia/chiemdieng/oauth2/Oauth2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package asia.chiemdieng.oauth2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
